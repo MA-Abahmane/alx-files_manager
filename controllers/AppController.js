c9b6cde98349eb27824958962227b912346597c6
Mohamed Amine <ma.abahmane@gmail.com>
2024-02-10 20:16:37 +0100

    push task 3

---
+++ controllers/AppController.js
@@ -0,0 +1,24 @@
+/* eslint-disable */
+const express = require('express')
+
+const redisClient = require('../utils/redis')
+const dbClient = require('../utils/db')
+
+
+const AppController = {
+
+    async getStatus(req, res) {
+        const redisStatus = await redisClient.isAlive()
+        const dbStatus = await dbClient.isAlive()
+        res.status(200).json({ "redis": redisStatus, "db": dbStatus })
+    },
+
+    async getStats(req, res) {
+        const userCount = await dbClient.nbUsers()
+        const fileCount = await dbClient.nbFiles()
+        res.status(200).json({ "users": userCount, "files": fileCount })
+    },
+
+}
+
+module.exports = AppController
---
+++ routes/index.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+const express = require('express')
+const AppController  = require('../controllers/AppController')
+
+const router = express.Router()
+
+
+router.get('/status', (req, res) => {
+    AppController.getStatus(req, res)
+})
+
+router.get('/stats', (req, res) => {
+    AppController.getStats(req, res)
+})
+
+module.exports = router
