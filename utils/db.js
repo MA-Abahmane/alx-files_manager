a0056a42e83b594a1ebbb6a89eab1a8a87ed50c2
Mohamed Amine <ma.abahmane@gmail.com>
2024-02-10 19:25:35 +0100

    push task 1

---
+++ utils/db.js
@@ -0,0 +1,40 @@
+const { MongoClient } = require('mongodb')
+
+
+class DBClient {
+
+    constructor() {
+        // If environment variable are undefined use default values
+        const host = process.env.DB_HOST || 'localhost'
+        const port = process.env.DB_PORT || 27017
+        const database = process.env.DB_DATABASE || 'files_manager'
+
+        const uri = `mongodb://${host}:${port}/${database}`
+        this.client = new MongoClient(uri)
+        this.client.connect((err) => {
+            if (err)
+                console.log('Mongo client connect Error:', err);
+        })
+    }
+
+    isAlive() {
+        // check is user is online
+        return this.client.isConnected()
+    }
+
+    async nbUsers() {
+        //  returns the number of documents in the collection users
+        const db = this.client.db()
+        return db.collection('users').countDocuments()
+    }
+
+    async nbFiles() {
+        // returns the number of documents in the collection files
+        const db = this.client.db()
+        return db.collection('files').countDocuments()
+    }
+
+}
+
+const dbClient =  new DBClient()
+module.exports = dbClient
